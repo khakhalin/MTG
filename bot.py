61852eb382f230b1f2193b0328098582fe4ec230
Bobby Mills <bobbyam115@gmail.com>
2018-12-20 18:36:25 -0500

    added danBot
    changed bot.decide to expect an argsorted pack. Added danbot sub class.

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-from random import randint
+from random import shuffle
+
 
 class Bot(object):
 
-    #Class CONSTANTS
-    #None for base bots but yours should go here in ALL_CAPS
-
+    # Class CONSTANTS
+    # None for base bots but yours should go here in ALL_CAPS
 
     def __init__(self):
         self.num_correct = 0
@@ -19,7 +19,7 @@ class Bot(object):
     with the drafting logic of you bot's subclass.
     '''
     def decide(self, draft_frame):
-        choice = self.__get_choice(draft_frame)
+        choice = self.get_choice(draft_frame)[-1]
 
         self.num_total += 1
 
@@ -28,20 +28,22 @@ class Bot(object):
 
         return choice
 
+
     '''
     INPUT: the input from decide
-    OUTPUT same as decide
+    OUTPUT: argsorted list of indices of a pack
     
     This is where your bot logic should go. In your bot subclass, overwrite this function
     with your own evaluation method. INPUTS and OUTPUTS must be standard across all bots.
     '''
     def __get_choice(self, draft_frame):
-        choice = randint(0, len(draft_frame[1])) - 1
-        return choice
-    #return tuple of number correct and total so far
+        return shuffle(draft_frame[1])
+
+    # return tuple of number correct and total so far
     def get_performance(self):
         return self.num_total, self.num_correct
-    #reset class variables
+
+    # reset class variables
     def clear_history(self):
         self.num_correct = 0
         self.num_total = 0
