ca6e41f93a2258afbc04593d3f7366b44c8a2603
danieljbrooks <daniel.brooks@alumni.caltech.edu>
2019-07-14 22:49:50 -0700

    Restructured folders.

--- bots/draftsimtools/__init__.py
+++ bots/draftsimtools/__init__.py
@@ -9,7 +9,9 @@ import pandas as pd
 
 from .load import *
 from .bot import *
-from .draftsim_bot_2018 import *
-from .draftsim_bot_2018_sgd import *
 from .bot_tester import *
 
+from . import bots
+
+#from .bots/draftsim_bot_2018 import *
+#from .bots/draftsim_bot_2018_sgd import *
