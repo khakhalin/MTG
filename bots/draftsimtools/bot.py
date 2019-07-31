ea68aafdd9fd45a6221f00da6d62a6f3e0765a91
danieljbrooks <daniel.brooks@alumni.caltech.edu>
2019-07-31 04:28:02 -0700

    Minor updates.

--- bots/draftsimtools/bot.py
+++ bots/draftsimtools/bot.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 class Bot(object):
 
     #Class CONSTANTS
@@ -12,14 +13,14 @@ class Bot(object):
 
 
     def decide(self, draft_frame):
-        '''
+        """
         INPUT: one draft frame in form [pack, collection, pick]
         OUTPUT: index of pack of chosen card
         NOTE: use list not tuple or dict for input
     
         This method is to be called by the testing script. Modify the get_choice method
         with the drafting logic of you bot's subclass.
-        '''
+        """
         choice = self.__get_choice(draft_frame)
 
         self.num_total += 1
@@ -31,23 +32,24 @@ class Bot(object):
 
 
     def __get_choice(self, draft_frame):
-        '''
+        """
         INPUT: the input from decide
         OUTPUT same as decide
     
         This is where your bot logic should go. In your bot subclass, overwrite this function
         with your own evaluation method. INPUTS and OUTPUTS must be standard across all bots.
-        '''
+        """
         choice = randint(0, len(draft_frame[1])) - 1
         return choice
 
 
     def get_performance(self):
-        ''' Return tuple of number of correct picks so far. '''
+        """ Return tuple of number of correct picks so far."""
         return self.num_total, self.num_correct
     
     
     def clear_history(self):
-        ''' Reset class variables. '''
+        """Reset class variables."""
         self.num_correct = 0
         self.num_total = 0
+
