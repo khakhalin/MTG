8ca8d1dd2e87ad8397cf1147d35fb481be0a8b5b
khakhalin <khakhalin@gmail.com>
2019-08-05 17:53:40 -0400

    tester: + toddler bot (raredrafter)

--- bots/draftsimtools/__init__.py
+++ bots/draftsimtools/__init__.py
@@ -11,7 +11,7 @@ from .load import *
 from .bot import *
 from .bot_tester import *
 
-from . import bots
+# from . import bots # As of Aug 5, generates an error. Is it obsolete now?
 
 #from .bots/draftsim_bot_2018 import *
 #from .bots/draftsim_bot_2018_sgd import *
